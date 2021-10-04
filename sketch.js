dfadf7bf718c07b92e25b322a842d4b621ff00cc
Sarang-R-119 <sarangvr@yahoo.com>
2021-10-03 20:39:52 -0400

    Error at this.velocity.add() (209)

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ class GameObject {
       'w           wwwww                      w',
       'w  wwww                wwwww          w',
       'w     www         wwwww                w',
-      'w           wwwww                      w',
+      'wp          wwwww                      w',
       'wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww',
     ]
 
@@ -30,6 +30,7 @@ class GameObject {
     this.instructions_state = false; // Checking if the instructions has loaded
     
     this.walls = [];
+    this.player;
   }
 
   initTilemap() {
@@ -39,6 +40,9 @@ class GameObject {
                 case 'w': 
                     this.walls.push(new Wall(j*20, i*20));
                     break; 
+                case 'p':
+                  this.player = new Player(j*20, i*20);
+                  break;
             }
         }
       }
@@ -60,7 +64,79 @@ function customBrick() {
   pop();
 }
 
+function customPlayer() {
+  push();
+    background(220, 220, 220, 0);
+    push();
+      fill(212, 175, 142);
+      rect(50, 0, 300, 400); // face
+    pop();
+  
+    push();
+      noStroke();
+      fill('#5B4343')
+      rect(50, 0, 300, 50); // top part of the hat
+      rect(0, 50, 400, 50); // bottom part of the hat
+    pop();
+
+    push();
+      rect(80, 150, 90, 40); // left eye
+      rect(230, 150, 90, 40); // right eye
+    pop();
+
+    push();
+      fill(0);
+      if(keyIsDown(RIGHT_ARROW) == true){
+        rect(130, 150, 40, 40); // turning right  
+      }
+      else if(keyIsDown(LEFT_ARROW)){
+       rect(80, 150, 40, 40); // turning left 
+      }
+      else {
+        rect(105, 150, 40, 40);
+      }
+    pop();
+
+    push();
+      fill(0);  
+      if(keyIsDown(RIGHT_ARROW)  == true){
+        rect(280, 150, 40, 40); // turning right
+      }
+      else if(keyIsDown(LEFT_ARROW)){
+        rect(230, 150, 40, 40); // turning left
+      }
+      else{
+        rect(255, 150, 40, 40); // turning left
+      }
+    pop();
+
+    push();
+      fill('#B67A45');
+      rect(175, 215, 50, 50); // nose
+    pop();
+
+    push();
+      fill('#24180E')
+      rect(50, 280, 300, 120); // beard
+    pop();
+
+    push();
+      fill(204, 146, 144);
+      noStroke();
+      rect(140, 330, 125, 30);
+      if(keyIsDown(RIGHT_ARROW)  == true){
+        rect(140, 310, 20, 20); // turning right
+      }
+      else if(keyIsDown(LEFT_ARROW)){
+        rect(245, 310, 20, 20); // turning left
+      }
+    pop();
+    custom_elements.push(get(0, 0, width, height));
+  pop();
+}
+
 function customElements() {
+  customPlayer();
   customBrick();
 }
 
@@ -74,9 +150,141 @@ class Wall{
   }
 
   draw() {
-    image(custom_elements[0], this.x, this.y, 20, 20);
+    image(custom_elements[1], this.x, this.y, 20, 20);
   }
 
+}
+
+class Player{
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.centerX = x + 10;
+    this.centerY = y + 10;
+    this.position = new p5.Vector(x, y);
+    this.velocity = new p5.Vector(0, 0);
+    this.acceleration = new p5.Vector(0, 0);
+    this.force = new p5.Vector(0, 0);
+    this.currFrame = frameCount;
+    this.jump = 0;
+  }
+
+  draw() {
+    image(custom_elements[0], this.x, this.y, 20,20); 
+
+     this.acceleration.set(0, 0);
+    }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  update() {
+    this.acceleration.set(0, 0);
+    
+//     if (this.walkForward === 1) {
+//       this.applyForce(walkForce);
+//     if (this.walkBackward === 1) {
+//       this.applyForce(backForce);
+// if (this.position.x > 410) {
+//   this.position.x = -10;
+// }
+// else if (this.position.x < -10) {
+//   this.position.x = 410;
+// }
+    
+    // Going to jump
+    if (this.jump === 2) {
+      
+      this.applyForce(jumpForce);
+      this.jump = 1;
+    
+    }
+    
+    // In air
+    if (this.jump > 0){
+      this.applyForce(gravity);
+    }
+  
+    this.velocity.add(this.acceleration);
+    
+    // Ground condition
+    if (this.velocity.y > 0 && this.check_collision_with_walls_Y(1)) {
+      this.position.y -= 1;
+      this.velocity = 0;
+      this.jump = 0;
+    }
+    
+    this.position.add(this.velocity);
+    this.acceleration.set(0, 0);
+  }
+  
+  move() {
+    var deltaX = 0;
+
+    if (keyIsDown(RIGHT_ARROW) && this.x < width - 10){
+        deltaX = 5;
+    }
+    if (keyIsDown(LEFT_ARROW) && this.x > 10){
+        deltaX = -5;
+    }
+
+    // Checking if player collides in X direction
+    if(this.check_collision_with_walls_X(deltaX) == true){
+      deltaX = 0;
+    }
+
+  // this.check_collision_with_diamonds(deltaX, deltaY);
+  
+  this.position.x += deltaX;
+  }
+
+  check_collision_with_walls_X(deltaX) {
+      
+    for (var i=0; i < gameObj.walls.length; i++) {
+
+        var horizontal_distance = abs(gameObj.walls[i].centerX - ((this.position.x + 10) + deltaX));
+
+        if(horizontal_distance <= 19.99) {
+          console.log('Collision with wall, xdist: ' + horizontal_distance);
+          return true;
+        }
+      }
+
+      return false;
+  }
+    
+  check_collision_with_walls_Y(deltaY) {
+      
+    for (var i=0; i < gameObj.walls.length; i++) {
+
+        var vertical_distance = abs(gameObj.walls[i].centerY - ((this.position.y + 10) + deltaY));
+        
+        if(vertical_distance <= 19.99) {
+          console.log('Collision with wall, ydist: ' + vertical_distance);
+          return true;
+        }
+      }
+
+      return false;
+  }
+  
+}
+
+var gravity, walkForce, backForce, jumpForce, jumpForce2;
+function keyPressed() {
+  if ((keyCode === 32) && (gameObj.player.jump === 0)) {
+    gameObj.player.jump = 2;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === RIGHT_ARROW) {
+    gameObj.player.walkForward = 0;
+  }
+  else if (keyCode === LEFT_ARROW) {
+    gameObj.player.walkBackward = 0;
+  }
 }
 
 class StartScreen{
@@ -247,6 +455,12 @@ function setup() {
   
   customElements();
   gameObj.initTilemap();
+
+  gravity = new p5.Vector(0, 0.15);
+  walkForce = new p5.Vector(0.1, 0);
+  backForce = new p5.Vector(-0.1, 0);
+  jumpForce = new p5.Vector(0, -5);
+  // jumpForce2 = new p5.Vector(0, -0.4);
 }
 
 function draw_walls() {
@@ -268,5 +482,8 @@ function draw() {
   }
   else if(gameObj.game_state) {
     draw_walls();
+    gameObj.player.draw();
+    gameObj.player.update();
+    gameObj.player.move();
   }
 }
